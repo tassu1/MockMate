4989dae5325c7a6150eaff1c54c444834234a39d
tassu1 <tassutahsee@gmail.com>
2026-06-08 14:41:16 +0530

    Initial MockMate setup

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./src/config/db.js";
+
+dotenv.config();
+connectDB()
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "MockMate API Running"
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
