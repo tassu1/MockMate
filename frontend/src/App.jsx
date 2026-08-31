d50ba25cfcac644b065b4eaf071f67ff2a75eba6
tassu1 <tassutahsee@gmail.com>
2026-08-31 23:47:38 +0530

    added routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
 import Auth from './pages/Auth';
+import Dashboard from './pages/Dashboard';
+import Interview from './pages/Interview';
+import Report from './pages/Report';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
@@ -10,6 +14,31 @@ function App() {
         {/* Pass the mode directly via route definition */}
         <Route path="/login" element={<Auth passmode="login" />} />
         <Route path="/signup" element={<Auth passmode="signup" />} />
+
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/interview/:id"
+          element={
+            <ProtectedRoute>
+              <Interview />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/interview/:id/report"
+          element={
+            <ProtectedRoute>
+              <Report />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
